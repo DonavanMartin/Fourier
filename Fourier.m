4856ea611bfba0f42bb5dcab6e4537c10e61d337
Donavan <mart.donavan@gmail.com>
2016-01-07 16:39:09 -0500

    Correction de quelques points

--- Fourier.m
+++ Fourier.m
@@ -1,12 +1,22 @@
+import findjobj.m.* %findobj librairie for scrolldown box
 import plan.m.*  % class definition file
 import square_signals.m.*  % class definition file
 
-%  Calcul Fourier V1.0 - Signaux carrées -
+%  Calcul Fourier V2.0 - Signaux carrées -
 %  Auteur : Donavan Martin
-%  5 Janvier 2016
+%  6 Janvier 2016
 %  Inspirée de : Mathématiques des systèmes et signaux à temps continu
 %  "les séries de Fourier" Roch Lefebvre, Professeur titulaire
 
+
+% Paramètre d'affichage
+% m = nombre de lignes
+% n = nombre de colonnes
+% p = position
+m = 4;
+n = 3;
+p = 1;
+K = 10;
 
 % ---------Signal carrée---------
 % Arguments : 
@@ -15,37 +25,41 @@ import square_signals.m.*  % class definition file
 %   amp haut,         [-inf inf]
 %   amp bas,          [-inf inf]  
 %   echantillons      [0 inf.]
-signal = square_signals(1, 0.25, 1, 0, 1000);
+signal = square_signals(0.050, 0.5, 1.0, 0.0, 1000.0);
 
 % -------- Affiche Signal --------
 % Arguments
-%   m (nombre de graphique par colonne) [1 à inf.]
-%   n (nombre de graphique par ligne)   [1 à inf.]
+%   m (nombre de graphique par lignes)  [1 à inf.]
+%   n (nombre de graphique par colonnes)[1 à inf.]
 %   p (position dans la matrice mXn)    [1 à  m*n]
-signal.plot_signal(4, 1, 1);
+signal.plot_signal(m, n, p);
+p = p+n;
 
 % -------- Affiche spectre amplitude --------
 % Arguments
 %   nb_harmoniques (nombre d'harmoniques shouaités) [1 à inf.]
-%   m (nombre de graphique par colonne)             [1 à inf.]
-%   n (nombre de graphique par ligne)               [1 à inf.]
+%   m (nombre de graphique par lignes)              [1 à inf.]
+%   n (nombre de graphique par colonnes)            [1 à inf.]
 %   p (position dans la matrice mXn)                [1 à  m*n]
-signal.plot_spectre_ampl(20,4,1,2); 
+signal.plot_spectre_ampl(K,m, n, p);
+p = p+n;
 
 % -------- Affiche spectre phases --------
 % Arguments
-%   m (nombre de graphique par colonne) [1 à inf.]
-%   n (nombre de graphique par ligne)   [1 à inf.]
-%   p (position dans la matrice mXn)    [1 à  m*n]
-signal.plot_spectre_phase(4,1,3); 
+%   m (nombre de graphique par lignes)   [1 à inf.]
+%   n (nombre de graphique par colonnes) [1 à inf.]
+%   p (position dans la matrice mXn)     [1 à  m*n]
+signal.plot_spectre_phase(m, n, p);
+p = p+n;
 
 % -------- Affiche k premiers harmoniques --------
 % Arguments
 %   k (nombre d'harmoniques)            [1 à inf.]
-%   m (nombre de graphique par colonne) [1 à inf.]
-%   n (nombre de graphique par ligne)   [1 à inf.]
+%   m (nombre de graphique par lignes)  [1 à inf.]
+%   n (nombre de graphique par colonnes)[1 à inf.]
 %   p (position dans la matrice mXn)    [1 à m*n ]
-signal.plot_sum_harmonique(20,4,1,4); 
+signal.plot_sum_harmonique(K,m, n, p);
+p = p+n;
 
 
 
--- square_signals.m
+++ square_signals.m
@@ -20,6 +20,9 @@ classdef square_signals < matlab.mixin.SetGet
         DC;                   % Xo (moyenne)
         X;                    % Coefficient de Fourier
         y;                    % Courbe des harmoniques
+        
+        %Propriétés Affichage
+        hp;
     end
     
     methods
@@ -27,7 +30,7 @@ classdef square_signals < matlab.mixin.SetGet
         % Arguments : Période ,rapport cyclique , amp haut, amp bas, nb d'échantillons
         function obj = square_signals(T_,r_cycl,amp_haut,amp_bas,echantillons_)
             obj.T = T_;                  
-            obj.w0 = 2*pi/obj.T;                
+            obj.w0 = (2*3.141592653)/obj.T;                
             obj.t_haut = r_cycl;                   
             obj.t_bas  = 1-r_cycl;                   
             obj.amp_haut = amp_haut;
@@ -36,18 +39,28 @@ classdef square_signals < matlab.mixin.SetGet
             
             % Attr. les valeurs de chaque temps t
             obj.dt = obj.T./obj.echantillons;            
-            obj.t  = 0:obj.dt:obj.T-obj.dt; 
+            obj.t  = 0:obj.dt:(obj.T-obj.dt); 
             
             % Attr. les valeur de x(t) pour chaque temps t
-            x_haut(1:T_*obj.t_haut*echantillons_) = amp_haut;  
-            x_bas(1:T_*obj.t_bas*echantillons_)   = amp_bas;   
+            x_haut(1:obj.t_haut*echantillons_) = amp_haut;  
+            x_bas(1:obj.t_bas*echantillons_)   = amp_bas;   
             obj.x = [x_haut x_bas];
+            
+            % Propriétés GUI
+            obj.hp = uipanel('Title',...
+                             'Série de Fourier : Onde Carré',...
+                             'FontSize',12,...
+                             'BackgroundColor','white',...
+                             'Position',[0 0 1 1]);
         end
         
         %Affiche le signal à la position d'une matric mXn 
-        function plot_signal(obj,m,n,p)                       
-            subplot(m,n,p); 
+        function plot_signal(obj,m,n,p)
+            subplot(m,n,[p,p+n-1], 'Parent', obj.hp);
             plot(obj.t,obj.x);                             % Affiche Signal
+            title(strjoin({'Signal carrée:';... 
+                          ' r.c.=';num2str(obj.t_haut/(obj.t_bas+obj.t_haut));...
+                          ' Période =';num2str(obj.T) }));
         end
         
         %Affiche le spectre d'amplitude 
@@ -55,22 +68,45 @@ classdef square_signals < matlab.mixin.SetGet
             obj.nb_harmonique = nb_harmonique;
           
             for k=1:nb_harmonique                       % e^-jkw0t)
-                u(k,:) = exp(-j*k*obj.w0*obj.t);      
+                x0 = exp(-1i*k*obj.w0.*obj.t);
+                x1 = x0 + eps(x0); 
+                u(k,:) = x1;      
             end
-            obj.DC = sum(obj.x)*obj.dt;                 % X(0)
-            for k=1:obj.nb_harmonique                   
-                obj.X(k) = sum(obj.x.*u(k,:))*obj.dt;   % X(k)
+                obj.DC = sum(obj.x)/obj.echantillons;            % X(0)
+                
+            for k=1:nb_harmonique
+                x0 = sum((obj.x.*u(k,:))*(1/obj.echantillons));
+                x1 = x0 + eps(x0); 
+                obj.X(k) = x1;   % X(k)
             end
-            subplot(m,n,p); 
+            
+
             obj.A = abs(obj.X);
-            stem(0:obj.nb_harmonique,[obj.DC obj.A]);   % Affiche Batonnêts
+            
+            % Affiche les valeurs (scroll box)
+            harmoniques = strtrim(cellstr(num2str((0:obj.nb_harmonique)'))');
+            Amplitudes  = strtrim(cellstr(num2str([obj.DC obj.A]'))');
+            list = strcat(harmoniques, {': '}, Amplitudes);
+            obj.affiche_text_box(list,m,n,p);
+            
+            % Affiche Batonnêts
+            subplot(m,n,[p+1,p+n-1]); 
+            stem(0:obj.nb_harmonique,[obj.DC obj.A]);
         end
         
         %Affiche le spectre d'amplitude 
         function obj =  plot_spectre_phase(obj,m,n,p)   
-            subplot(m,n,p); 
             obj.P = angle(obj.X);
-            stem(0:obj.nb_harmonique,[obj.DC obj.P]);   % Affiche Batonnêts
+            
+             % Affiche les valeurs (scroll box)
+            harmoniques = strtrim(cellstr(num2str((0:obj.nb_harmonique)'))');
+            phases  = strtrim(cellstr(num2str([0 obj.P]'))');
+            list = strcat(harmoniques, {': '}, phases);
+            obj.affiche_text_box(list,m,n,p);
+            
+            % Affiche Batonnêts
+            subplot(m,n,[p+1,p+n-1]); 
+            stem(0:obj.nb_harmonique,[0 obj.P]);   
         end
         
         %Affiche la somme des k premiers harmoniques
@@ -82,8 +118,25 @@ classdef square_signals < matlab.mixin.SetGet
                 obj.y = obj.y + 2*obj.A(k)*cos(k*obj.w0*obj.t + obj.P(k));
             end
 
+            subplot(m,n,[p,p+n-1]);
+            plot(obj.t,obj.y);                         % Affiche Harmonique
+            title(strjoin({'Somme des ';num2str(K);' harmoniques'}));
+        end
+    end
+    methods (Access = private)
+         %Affiche un textbox a droite du graphique
+        function affiche_text_box(~,str,m,n,p)
+
             subplot(m,n,p);
-            plot(obj.t,obj.y);                         % Affiche Harmonique  
+            axis off; 
+            list_string = strjoin(str,'\n');
+            %# GUI with multi-line editbox
+            position_y = 1+((p-1)/n);
+            set_y = ((m)-position_y)/m;
+            uicontrol(figure(1), 'Style','edit', 'FontSize',9, ...
+                'Min',0, 'Max',2, 'HorizontalAlignment','left', ...
+                'Units','normalized', 'Position',[0.4/n (0.18/m)+set_y 0.65/n 0.60/m], ...
+                'String',list_string);
         end
     end 
 end
